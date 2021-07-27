a82ba116a28bce31d4a8bb43ed65fbd4b449f1a1
AustereTriceratops <gatewaylevi@gmail.com>
2021-07-26 22:49:45 -0500

    refactoring event methods, camera rotation partially implemented but not working

--- RayMarching/main.js
+++ RayMarching/main.js
@@ -8,7 +8,7 @@ var geometry, material, mesh;
 // Main ================================================
 function main()
 {
-	Simulator.Setup();
+	Simulator.setup();
 
 	// create plane of shader
 	geometry = new THREE.PlaneBufferGeometry(2, 2);
@@ -25,83 +25,14 @@ function main()
 	var v = new THREE.Quaternion(1, 0, 0, 0);
 
 	// ANIMATE ==================
-	Simulator.Animate();
+	Simulator.animate();
 }
 
 
-// TODO: make this work with multiple keys down
-// TODO: move uniform updates to another function called during animate()
-function handleInput(event)
-{
-	if (event.keyCode == 87)
-	{
-		var cameraYDirection = Simulator.cameraY.clone().multiplyScalar(0.01)
-		Simulator.cameraPos.add(cameraYDirection);
-
-		Simulator.updates['cameraPos'] = true;
-	}
-	if (event.keyCode == 65)
-	{
-		var cameraXDirection = Simulator.cameraX.clone().multiplyScalar(0.004)
-		Simulator.cameraPos.sub(cameraXDirection);
-
-		Simulator.uniforms['cameraPos']['value'] = Simulator.cameraPos;
-	}
-	if (event.keyCode == 83)
-	{
-		var cameraYDirection = Simulator.cameraY.clone().multiplyScalar(0.01)
-		Simulator.cameraPos.sub(cameraYDirection);
-
-		Simulator.uniforms['cameraPos']['value'] = Simulator.cameraPos;
-	}
-	if (event.keyCode == 68)
-	{
-		var cameraXDirection = Simulator.cameraX.clone().multiplyScalar(0.004)
-		Simulator.cameraPos.add(cameraXDirection);
-
-		Simulator.uniforms['cameraPos']['value'] = Simulator.cameraPos;
-	}
-	if (event.keyCode == 32)
-	{
-		var cameraZDirection = Simulator.cameraZ.clone().multiplyScalar(0.004)
-		Simulator.cameraPos.add(cameraZDirection);
-
-		Simulator.uniforms['cameraPos']['value'] = Simulator.cameraPos;
-	}
-	if (event.keyCode == 16)
-	{
-		var cameraZDirection = Simulator.cameraZ.clone().multiplyScalar(0.004)
-		Simulator.cameraPos.sub(cameraZDirection);
-
-		Simulator.uniforms['cameraPos']['value'] = Simulator.cameraPos;
-	}
-	if (event.keyCode == 27)
-	{
-		Simulator.trackMouse = !Simulator.trackMouse;
-		console.log(Simulator.trackMouse);
-	}
-
-	if (event.keyCode == 37)
-	{
-		console.log("Left arrow key down, rotate camera about its Z axis");
-	}
-	if (event.keyCode == 38)
-	{
-		console.log("Up arrow key down, rotate camera about its X axis");
-	}
-	if (event.keyCode == 39)
-	{
-		console.log("Right arrow key down, rotate camera about its Z axis");
-	}
-	if (event.keyCode == 40)
-	{
-		console.log("Down arrow key down, rotate camera about its X axis");
-	}
-}
-
-window.addEventListener('resize', Simulator.WindowResize, false);
-window.addEventListener('mousemove', Simulator.MouseMove, false);
-window.addEventListener('keydown', handleInput, false);
+window.addEventListener('resize', Simulator.windowResize, false);
+window.addEventListener('mousemove', Simulator.mouseMove, false);
+window.addEventListener('click', Simulator.mouseClick, false);
+window.addEventListener('keydown', Simulator.handleInput, false);
 
 // call to main
 main();
--- RayMarching/simulator.js
+++ RayMarching/simulator.js
@@ -5,6 +5,8 @@ class Simulator
 	static zoom = 1.0;
 	static offset = new THREE.Vector2(-0.50*Simulator.aspect, -0.5);
 	static uniforms;
+	static firstMouseMove = true;
+	static mouseMoved = true;
 
 	// mouse data
 	static mouseX = 0.0;
@@ -26,7 +28,7 @@ class Simulator
 
 
     // SETUP ================================================
-	static Setup()
+	static setup()
 	{
 		Simulator.renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild( Simulator.renderer.domElement );
@@ -56,16 +58,24 @@ class Simulator
 
 
 	// ANIMATION ================================================
-	static Animate()
+	static animate()
 	{
-		Simulator.UpdateUniforms();
+		Simulator.updateUniforms();
 
 		Simulator.renderer.render(Simulator.scene, Simulator.camera);
-		requestAnimationFrame(Simulator.Animate);
+		requestAnimationFrame(Simulator.animate);
 	}
 
-	static UpdateUniforms()
+	static updateUniforms()
 	{
+		console.log(Simulator.mouseMoved);
+		/**
+		 * Simulator.updates flags which uniforms need to be updated each frame
+		 * This method runs through that dictionary and updates the shader
+		 * uniforms which have been flagged for updating.
+		 * 
+		 * Running this every frame is more efficient than running it after every event.
+		 */
 		for (var key in Object.keys(Simulator.updates))
 		{
 			if (Simulator.updates[key])
@@ -73,12 +83,47 @@ class Simulator
 				Simulator.uniforms[key]["value"] = Simulator[key];
 				Simulator.updates[key] = false;
 			}
+
+		}
+		
+		if (Simulator.mouseMoved)
+		{
+			Simulator.mouseMoved = false;
+
+			if ( !Simulator.firstMouseMove)
+			{
+				// rotate the shader camera
+				// TODO: maybe take new declarations out of this method
+				var zRotation = new THREE.Quaternion();
+				zRotation.setFromAxisAngle(Simulator.cameraZ, Simulator.mouseXDelta/1000.0);
+				
+				var xRotation = new THREE.Quaternion();
+				xRotation.setFromAxisAngle(Simulator.cameraX, Simulator.mouseYDelta/1000.0);
+	
+				var totalRotation = new THREE.Quaternion();
+				totalRotation.multiplyQuaternions(xRotation, zRotation);
+	
+				Simulator.cameraX.applyQuaternion(totalRotation);
+				Simulator.cameraY.applyQuaternion(totalRotation);
+				Simulator.cameraZ.applyQuaternion(totalRotation);
+	
+				// set mouse position and reset the difference trackers
+				Simulator.mouseX += Simulator.mouseXDelta;
+				Simulator.mouseY += Simulator.mouseYDelta;
+	
+				Simulator.mouseXDelta = 0.0;
+				Simulator.mouseYDelta = 0.0;
+			}
+			else
+			{
+				Simulator.firstMouseMove = false;
+			}
 		}
 	}
 
 
 	// EVENTS ================================================
-	static WindowResize()
+	static windowResize()
 	{
 		Simulator.aspect = window.innerWidth / window.innerHeight;
 		Simulator.camera.aspect =  aspect;
@@ -86,11 +131,89 @@ class Simulator
 		Simulator.renderer.setSize( window.innerWidth, window.innerHeight-2);
 	}
 
-	static MouseMove(event)
+	static mouseMove(event)
 	{
 		if (Simulator.trackMouse)
 		{
-			console.log("Mouse");
+			// track how much the mouse has changed from the last animation frame
+			Simulator.mouseXDelta = event.clientX - Simulator.mouseX;
+			Simulator.mouseYDelta = event.clientY - Simulator.mouseY;
+
+			Simulator.mouseMoved = true;
+		}
+	}
+
+	static mouseClick(event)
+	{
+		// click the mouse to have the camera follow/unfollow the mouse
+		Simulator.trackMouse = !Simulator.trackMouse;
+
+		console.log("mouse clicked");
+
+		Simulator.mouseX = event.clientX;
+		Simulator.mouseY = event.clientY;
+	}
+
+	// TODO: make this work with multiple keys down
+	static handleInput(event)
+	{
+		if (event.keyCode == 87)
+		{
+			var cameraYDirection = Simulator.cameraY.clone().multiplyScalar(0.01)
+			Simulator.cameraPos.add(cameraYDirection);
+
+			Simulator.updates['cameraPos'] = true;
+		}
+		if (event.keyCode == 65)
+		{
+			var cameraXDirection = Simulator.cameraX.clone().multiplyScalar(0.004)
+			Simulator.cameraPos.sub(cameraXDirection);
+
+			Simulator.updates['cameraPos'] = true;
+		}
+		if (event.keyCode == 83)
+		{
+			var cameraYDirection = Simulator.cameraY.clone().multiplyScalar(0.01)
+			Simulator.cameraPos.sub(cameraYDirection);
+
+			Simulator.updates['cameraPos'] = true;
+		}
+		if (event.keyCode == 68)
+		{
+			var cameraXDirection = Simulator.cameraX.clone().multiplyScalar(0.004)
+			Simulator.cameraPos.add(cameraXDirection);
+
+			Simulator.updates['cameraPos'] = true;
+		}
+		if (event.keyCode == 32)
+		{
+			var cameraZDirection = Simulator.cameraZ.clone().multiplyScalar(0.004)
+			Simulator.cameraPos.add(cameraZDirection);
+
+			Simulator.updates['cameraPos'] = true;
+		}
+		if (event.keyCode == 16)
+		{
+			var cameraZDirection = Simulator.cameraZ.clone().multiplyScalar(0.004)
+			Simulator.cameraPos.sub(cameraZDirection);
+
+			Simulator.updates['cameraPos'] = true;
+		}
+		if (event.keyCode == 37)
+		{
+			console.log("Left arrow key down, rotate camera about its Z axis");
+		}
+		if (event.keyCode == 38)
+		{
+			console.log("Up arrow key down, rotate camera about its X axis");
+		}
+		if (event.keyCode == 39)
+		{
+			console.log("Right arrow key down, rotate camera about its Z axis");
+		}
+		if (event.keyCode == 40)
+		{
+			console.log("Down arrow key down, rotate camera about its X axis");
 		}
 	}
 }
